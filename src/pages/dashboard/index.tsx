365ee52dc3a5a8b23b85d0f009a2249562249fb8
roberio <roberio-almeida@hotmail.com>
2023-04-18 15:12:37 -0300

    feat: criação do dashboard para todos os serviços

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,20 @@
+import { serviceData } from '@/pages/api/services';
+import { GetServerSideProps } from 'next';
+
+type PageProps = {
+  data: serviceData[];
+};
+
+export default function DashboardPage({ data }: PageProps) {
+  return <Dashboard data={data} />;
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const res = await fetch('http://localhost:3000/api/services');
+  const data = await res.json();
+  return {
+    props: {
+      data,
+    },
+  };
+};
