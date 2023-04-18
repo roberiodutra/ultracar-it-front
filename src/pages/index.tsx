41d7408ebbb83771c853266662ad31ee16632c84
roberio <roberio-almeida@hotmail.com>
2023-04-18 01:59:40 -0300

    feat: adiciona botões de navegação na home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,26 +17,35 @@ export default function Home() {
     });
 
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-2'>
+    <main className='flex min-h-screen flex-col items-center p-2'>
       <Head>
         <title>Ultracar IT Front</title>
       </Head>
-      <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
-        <a
-          className='pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0'
-          href='/'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <Image
-            src='/big-logo.png'
-            alt='Ultracar Big Logo'
-            width={100}
-            height={24}
-            priority
-          />
-          <span>text</span>
-        </a>
+      <div className='drop-shadow-xl'>
+        <Image
+          src='/big-logo.png'
+          alt='Ultracar Big Logo'
+          width={1024}
+          height={0}
+          priority
+        />
+      </div>
+      <div className='drop-shadow-xl pt-2 text-center sm:max-sm:flex container'>
+        <button className='opacity-75 m-2 w-80 bg-indigo-400 hover:bg-blue-400 text-slate-900 font-bold py-2 px-20 border-b-4 border-slate-600 hover:border-blue-500 rounded'>
+          BUSCAR CLIENTE
+        </button>
+        <button className='opacity-75 m-2 w-80 bg-blue-400 hover:bg-indigo-400 text-slate-900 font-bold py-2 px-20 border-b-4 border-slate-600 hover:border-blue-500 rounded'>
+          REALIZAR SERVIÇO
+        </button>
+      </div>
+      <div className='mt-2 drop-shadow-xl'>
+        <Image
+          src='/bg.jpg'
+          alt='footer home background'
+          width={1024}
+          height={0}
+          priority
+        />
       </div>
     </main>
   );
