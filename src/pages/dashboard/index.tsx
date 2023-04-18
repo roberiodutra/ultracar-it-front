285d51ef117b5cec1ca1a53e5896b943a552fc51
roberio <roberio-almeida@hotmail.com>
2023-04-18 19:37:07 -0300

    refactor: mudando o mock da pagina de dashboard para github actions

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,6 @@
 import Dashboard from '@/components/dashboard';
 import { serviceData } from '@/pages/api/services';
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 
 type PageProps = {
   data: serviceData[];
@@ -10,9 +10,36 @@ export default function DashboardPage({ data }: PageProps) {
   return <Dashboard />;
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const res = await fetch('http://localhost:3000/api/services');
-  const data = await res.json();
+export const getStaticProps: GetStaticProps = async () => {
+  const data = [
+    {
+      id: 1,
+      services: [
+        { name: 'Troca de óleo', category: 'Mechanics', price: '' },
+        { name: 'Revisão', category: 'Eletric', price: '' },
+      ],
+      status: 'Finished',
+      start_date: '2018-05-05T11:50:55',
+      end_date: '2018-05-05T14:50:55',
+    },
+    {
+      id: 2,
+      services: [{ name: 'Alinhamento', category: 'Mechanics', price: '' }],
+      status: 'Canceled',
+      start_date: '2018-05-05T11:50:55',
+      end_date: '2018-05-05T14:50:55',
+    },
+    {
+      id: 3,
+      services: [
+        { name: 'Balanceamento', category: 'Mechanics', price: '' },
+        { name: 'Reparação', category: 'Eletric', price: '' },
+        { name: 'Troca de peça', category: 'Eletric', price: '50.00' },
+      ],
+      status: 'Ongoing',
+    },
+  ];
+
   return {
     props: {
       data,
